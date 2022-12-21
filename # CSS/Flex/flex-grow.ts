88b3b19ab7ed3aa1a53181c13a1fac2d87bf1de5
xilinyo <xilinyo.git>
2022-12-21 09:44:40 +0800

    feat: css

--- # CSS/Flex/flex-grow.ts
+++ # CSS/Flex/flex-grow.ts
@@ -1,5 +1,5 @@
 module CSS {
-  /** @分配剩余空间 box 500 item 100 150 100 剩余 150 即如何分配剩余的 150 */
+  /** @分配剩余空间 box 500 items 100 150 100 剩余 150 即如何分配剩余的 150 */
   interface Syntax {
     "flex-grow": () => number;
   }
